e7f0c22eb830f2831d36dc6cdebdb60a7b918e16
Jeffrey Ness <jness@flip-edesign.com>
2011-06-28 18:00:40 -0500

    adding Demonoid login to start a session

--- Providers/demonoid.py
+++ Providers/demonoid.py
@@ -1,8 +1,9 @@
 # This is an addtional provider for http://www.demonoid.me/
 # which supports checking a users RSS feed for new torrents
 
-import urllib2
+import urllib2, urllib
 import re
+from Libs.config import get_config
 
 def userRSS(user, series):
     '''Search a prefered User's RSS feed for episodes similar to your series'''
@@ -12,6 +13,16 @@ def userRSS(user, series):
 
     match = re.compile('<title>TV - (%s .*)</title>.*<link>(.*)</link>' % series, re.IGNORECASE).findall(rss)
     return match
+
+def login():
+    c = get_config()
+    u = 'http://www.demonoid.me/account_handler.php'
+    req = urllib2.Request(u)
+    post = {'nickname': c['demonoid_username'], 'password': c['demonoid_password'], 'withq': '1', 'returnpath': '/?rel=1309301236'}
+    req.add_data(urllib.urlencode(post))
+    content = urllib2.urlopen(req).read()
+    return
+
 
 def episodes(s):
     '''Search over the results of userRSS and extract episode numbers,
@@ -28,5 +39,9 @@ def episodes(s):
                 match = re.search('<a href="(.*)">Click here to download the torrent</a>', page)
                 if match:
                     epis.append(('http://www.demonoid.me' + match.group(1), epnum.group(1)))
+
+    # get a session so we can download
+    login()
+
     return epis
 
